69b63b2bb0f93c8d9ec4cd963a98508fdea26217
Beatus Simon <beatussimon49@gmail.com>
2025-01-15 19:48:33 +0300

    Make api calls, navbar and style all the componets

--- src/components/FoodItem.jsx
+++ src/components/FoodItem.jsx
@@ -1,9 +1,15 @@
+import styles from './foodItem.module.css'
+
 export default function FoodItem({food}) {
   return (
-    <div>
-        <img src={food.img} alt="" />
-        <h1>{food.title}</h1>
-        <button>View Recipe</button>
+    <div className={styles.itemContainer}>
+        <img className={styles.itemImage} src={food.image} alt="" />
+        <div className={styles.itemContent}>
+            <p>{food.title}</p>
+        </div>
+        <div className={styles.buttonContainer}>
+            <button className={styles.itemButton}>View Recipe</button>
+        </div>
     </div>
   )
 }
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,22 +1,25 @@
 import { useEffect, useState } from "react"
+import styles from './search.module.css'
 
-const URL = 'www.google.com/api'
-const API_KEY = '2j3h5lj6h63ll34l6664llj'
+const URL = 'https://api.spoonacular.com/recipes/complexSearch'
+const API_KEY = '720b8436255344b4b49d0bb6cbdee1f7'
 
 export default function Search({foodData, setFoodData}) {
     const [query, setQuery] = useState("pizza")
 
     useEffect(()=>{
         async function fetchFood(){
-            const res = await fetch(`${URL}?query=${query}&apikey=${API_KEY}`)
+            const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`)
             const data = await res.json()
             setFoodData(data.results)
+            console.log(data.results)
         }
+        fetchFood()
     },[query])
 
   return (
-    <div>
-        <input type="text" onChange={(e)=>setQuery(e.target.value)} value={query}/>
+    <div className={styles.searchContainer}>
+        <input className={styles.input} type="text" onChange={(e)=>setQuery(e.target.value)} value={query}/>
     </div>
   )
 }
